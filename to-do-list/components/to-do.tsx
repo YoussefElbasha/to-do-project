68e70bbf774f3eb7bd69e237275b7c602e1d1ce5
Youssef Saad <youssef.elbasha.saad@gmail.com>
2023-02-03 14:29:46 +0200

    modified to-do components

--- to-do-list/components/to-do.tsx
+++ to-do-list/components/to-do.tsx
@@ -1,40 +1,63 @@
-import { FC, FormEvent, useState } from "react";
-import styles from "../styles/ToDo.module.css";
+import { FC, FormEvent, useState } from 'react'
+import styles from '../styles/ToDo.module.css'
+import todoStatus from '../constants/status'
 
 interface props {
-  deleteHandler: () => void;
-  submitHandler: (text: string) => void;
-  input?: any;
+	deleteHandler: () => void
+	inputSubmitHandler: (text: string) => void
+	statusSubmitHandler: (status: string) => void
+	input: any
+	status: todoStatus
 }
 
-const Todo: FC<props> = ({ deleteHandler, submitHandler, input }) => {
-  const [text, setText] = useState<string>(input);
+const Todo: FC<props> = ({
+	deleteHandler,
+	inputSubmitHandler,
+	statusSubmitHandler,
+	input,
+	status,
+}) => {
+	const [text, setText] = useState<string>(input)
 
-  const handleSumbit = (e: FormEvent<HTMLFormElement>) => {
-    submitHandler(text);
-    e.preventDefault();
-  };
+	const handleInputSubmit = (e: any) => {
+		setText(e.currentTarget.value)
+		inputSubmitHandler(text)
+		e.preventDefault()
+	}
 
-  const handleChange = (e: any) => {
-    setText(e.currentTarget.value);
-  };
+	const handleStatusSubmit = (e: any) => {
+		statusSubmitHandler(e.currentTarget.value)
+	}
 
-  return (
-    <div>
-      <form onSubmit={handleSumbit}>
-        <input
-          type="text"
-          aria-label="placeholder"
-          placeholder="edit me"
-          onChange={handleChange}
-          value={text}
-        />
-        <button onClick={deleteHandler} type="button">
-          Delete
-        </button>
-      </form>
-    </div>
-  );
-};
+	// const handleChange = (e: any) => {
+	// 	setText(e.currentTarget.value)
+	// }
 
-export default Todo;
+	return (
+		<div>
+			<form /* onSubmit={handleInputSubmit} */>
+				<input
+					type="text"
+					aria-label="placeholder"
+					placeholder="edit me"
+					onChange={handleInputSubmit}
+					value={text}
+				/>
+				<select
+					onChange={handleStatusSubmit}
+					name="status"
+					defaultValue={status}
+				>
+					<option value="IN_PROGRESS">In progress</option>
+					<option value="COMPLETED">Completed</option>
+					<option value="ON_HOLD">On hold</option>
+				</select>
+				<button onClick={deleteHandler} type="button">
+					Delete
+				</button>
+			</form>
+		</div>
+	)
+}
+
+export default Todo
--- to-do-list/pages/personal-list.tsx
+++ to-do-list/pages/personal-list.tsx
@@ -1,50 +1,97 @@
-import { useState, useEffect } from "react";
-import Todo from "../components/to-do";
+import { useState, useEffect } from 'react'
+import Todo from '../components/to-do'
+import idGenerator from '../lib/idGenerator'
+import todoStatus from '../constants/status'
+
+interface todoObject {
+	text: string
+	id: string
+	status: todoStatus
+}
 
 const PersonalList = () => {
-  const [todos, setTodos] = useState<string[]>([]);
+	const [todos, setTodos] = useState<todoObject[]>([])
 
-  useEffect(() => {
-    console.log(todos);
-  }, [todos]);
+	useEffect(() => {
+		setTodos(JSON.parse(sessionStorage.getItem('todo_list') || '[]'))
+	}, [])
 
-  const submitHandler = (text: string, index: number) => {
-    const tempTodos = todos;
-    tempTodos[index] = text;
-    setTodos(tempTodos);
-    console.log(todos);
-  };
+	useEffect(() => {
+		console.log(todos)
+	}, [todos])
 
-  const deleteHandler = (deletedKey: number) => {
-    const tempTodos = todos.filter((todo, index) => index !== deletedKey);
-    setTodos(tempTodos);
-  };
+	const inputSubmitHandler = (text: string, id: string) => {
+		const tempTodos = todos
+		tempTodos.forEach((todo) => {
+			if (todo.id === id) {
+				todo.text = text
+			}
+		})
+		setTodos(tempTodos)
+		console.log(todos)
+	}
 
-  const clickHandler = (event: any) => {
-    setTodos([...todos, ""]);
-  };
+	const statusSubmitHandler = (status: string, id: string) => {
+		const tempTodos = todos
+		tempTodos.forEach((todo) => {
+			if (todo.id === id) {
+				todo.status = status as todoStatus
+			}
+		})
+		setTodos(tempTodos)
+		console.log(todos)
+	}
 
-  const save = () => {
-    sessionStorage.setItem("todo_list", todos.toString());
-    console.log("Storage: " + sessionStorage.getItem("todo_list")?.split(","));
-  };
+	const deleteHandler = (deletedId: string) => {
+		const tempTodos = todos.filter((todo) => todo.id !== deletedId)
+		setTodos(tempTodos)
+	}
 
-  return (
-    <div>
-      <button onClick={clickHandler}>Add to-do task</button>
-      <ul>
-        {todos.map((todo, index) => (
-          <Todo
-            key={index}
-            input={todo}
-            deleteHandler={() => deleteHandler(index)}
-            submitHandler={(text: string) => submitHandler(text, index)}
-          />
-        ))}
-      </ul>
-      <button onClick={save}>Save</button>
-    </div>
-  );
-};
+	const clickHandler = (event: any) => {
+		setTodos([
+			...todos,
+			{ text: '', id: idGenerator(), status: todoStatus.IN_PROGRESS },
+		])
+	}
 
-export default PersonalList;
+	const save = () => {
+		sessionStorage.setItem('todo_list', JSON.stringify(todos))
+		console.log(
+			'Storage: ',
+			JSON.parse(sessionStorage.getItem('todo_list') || '[]')
+		)
+	}
+
+	const clear = () => {
+		sessionStorage.setItem('todo_list', JSON.stringify([]))
+		console.log('Storage: ', sessionStorage.getItem('todo_list'))
+	}
+
+	return (
+		<div>
+			<button onClick={clickHandler}>Add to-do task</button>
+			{
+				<ul>
+					{todos.map((todo) => (
+						<Todo
+							key={todo.id}
+							input={todo.text}
+							status={todo.status}
+							deleteHandler={() => deleteHandler(todo.id)}
+							inputSubmitHandler={(text: string) =>
+								inputSubmitHandler(text, todo.id)
+							}
+							statusSubmitHandler={(status: string) =>
+								statusSubmitHandler(status, todo.id)
+							}
+						/>
+					))}
+				</ul>
+			}
+			<button onClick={save}>Save</button>
+			<button onClick={clear}>Clear Storage</button>
+		</div>
+	)
+}
+
+export default PersonalList
